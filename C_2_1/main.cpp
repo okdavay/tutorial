6ed10485baa4da52fbc61823397b8bf14ce337ad
NikitaDotIvanov64 <45095181+NikitaDotIvanov64@users.noreply.github.com>
2019-12-08 19:10:36 +0300

    Удалил лишние файлы подредактировал ignore

--- C_2_1/main.cpp
+++ C_2_1/main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
-//#define k_F 7,481
+// f на конце говорит о том что число будет типа float
+#define k_F 7.481f
 
-int main()
-{
-const float k_F = 7.481F;
-std::cout<<"РІРІРµРґРёС‚Рµ С‡РёСЃР»Рѕ РіР°Р»Р»РѕРЅРѕРІ : \n ";
-float gal =0;
-std::cin>>gal;
-float result=gal/k_F;
-std::cout<< result<<"\b"<<"РєСѓР±РёС‡РµСЃРєРёС… С„СѓРЅС‚РѕРІ \n";
-return 0;
+int main() {
+  std::cout << "РІРІРµРґРёС‚Рµ С‡РёСЃР»Рѕ РіР°Р»Р»РѕРЅРѕРІ :";
+
+  float gal = 0;
+  std::cin >> gal;
+
+  float result = gal / k_F;
+
+  std::cout << result << "\b"
+            << "РєСѓР±РёС‡РµСЃРєРёС… С„СѓРЅС‚РѕРІ \n";
+
+  return 0;
 }
+
+// PS имена лучше давать более осмысленными. В маленьких приложениях не
+// критично.
